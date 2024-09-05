cef3569684f41a6385843fec4b69481aace4a0bb
Chayney <chunchunmaru33333@gmail.com>
2024-09-05 22:01:58 +0900

    会員登録ページの編集

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -7,17 +7,15 @@
 @section('content')
 <div class="register__content">
   <div class="register-form__heading">
-    <h2>会員登録</h2>
+    <span class="heading-name">Registration</span>
   </div>
   <form class="form" action="/register" method="post">
     @csrf
     <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">お名前</span>
-      </div>
+      <img class="label_image" src="{{ asset('image/person.png') }}">
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="text" name="name" value="{{ old('name') }}" />
+          <input type="text" name="name" value="{{ old('name') }}" placeholder="Username" />
         </div>
         <div class="form__error">
           @error('name')
@@ -27,12 +25,10 @@
       </div>
     </div>
     <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">メールアドレス</span>
-      </div>
+      <img class="label_image" src="{{ asset('image/mail.png') }}">
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="email" name="email" value="{{ old('email') }}" />
+          <input type="email" name="email" value="{{ old('email') }}" placeholder="Email" />
         </div>
         <div class="form__error">
           @error('email')
@@ -42,12 +38,10 @@
       </div>
     </div>
     <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">パスワード</span>
-      </div>
+      <img class="label_image" src="{{ asset('image/key.png') }}">
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="password" name="password" />
+          <input type="password" name="password" placeholder="Password" />
         </div>
         <div class="form__error">
           @error('password')
@@ -56,9 +50,7 @@
         </div>
       </div>
     </div>
-    <div class="form__button">
-      <button class="form__button-submit" type="submit">登録</button>
-    </div>
+    <button class="form__button-submit" type="submit">登録</button>
   </form>
 </div>
 @endsection
