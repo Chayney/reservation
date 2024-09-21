d1f22a9dac35411eeecf774199138979ce7ec9b3
Chayney <chunchunmaru33333@gmail.com>
2024-09-21 18:43:51 +0900

    レスポンシブデザインの調整

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,27 +12,25 @@
 
 <body>
   <header class="header">
-    <div class="header__inner">
-      <div class="nav-humberger">
-        <input id="drawer_input" class="drawer_hidden" type="checkbox">
-        <label for="drawer_input" class="drawer_open"><span></span></label>
-      <nav class="nav_content">
-          <ul class="nav_list">
-            <li class="nav_item"><a href="/">Home</a></li>
-            @if (Request::is('register'))
-              <li class="nav_item"><a href="{{ url('login') }}">Login</a></li>
-              <li class="nav_item"><a href="{{ url('mypage') }}">Mypage</a></li>
-            @else
-              <li class="nav_item"><a href="{{ url('register') }}">Registration</a></li>
-              <li class="nav_item"><a href="{{ url('mypage') }}">Mypage</a></li>
-            @endif
-          </ul>
-      </nav>
-      </div>
-      <a class="header__logo" href="/">
-        Rese
-      </a>
+    <div class="nav-humberger">
+      <input id="drawer_input" class="drawer_hidden" type="checkbox">
+      <label for="drawer_input" class="drawer_open"><span></span></label>
+    <nav class="nav_content">
+        <ul class="nav_list">
+          <li class="nav_item"><a href="/">Home</a></li>
+          @if (Request::is('register'))
+            <li class="nav_item"><a href="{{ url('login') }}">Login</a></li>
+            <li class="nav_item"><a href="{{ url('mypage') }}">Mypage</a></li>
+          @else
+            <li class="nav_item"><a href="{{ url('register') }}">Registration</a></li>
+            <li class="nav_item"><a href="{{ url('mypage') }}">Mypage</a></li>
+          @endif
+        </ul>
+    </nav>
     </div>
+    <a class="header__logo" href="/">
+      Rese
+    </a>
   </header>
 
   <main>
