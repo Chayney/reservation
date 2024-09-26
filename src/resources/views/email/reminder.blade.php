f039a226882246b5f9c1e5b1e207147d486eb06d
Chayney <chunchunmaru33333@gmail.com>
2024-09-26 22:41:48 +0900

    カラム名の修正

--- src/resources/views/email/reminder.blade.php
+++ src/resources/views/email/reminder.blade.php
@@ -5,13 +5,13 @@
 </head>
 <body>
     <h1>Reseからのお知らせ</h1>
-    <h2> {{ $reservation->reserveUser->name }} 様</h2>
+    <h2> {{ $reservation['reserveUser']['name'] }} 様</h2>
     <p>以下の内容で予約を受け付けています。</p>
     <ul>
-        <li>店舗名： {{ $reservation->reserveShop->shop }}</li>
-        <li>予約日： {{ \Carbon\Carbon::parse($reservation->date)->locale('ja')->isoFormat('YYYY-MM-DD (dd)') }}</li>
-        <li>予約時間： {{ date('H:i',strtotime($reservation->time)) }}</li>
-        <li>予約人数： {{ $reservation->person }}</li>
+        <li>店舗名： {{ $reservation['reserveShop']['shop'] }}</li>
+        <li>予約日： {{ $reservation['date'] }}</li>
+        <li>予約時間： {{ $reservation['time']->format('H:i') }}</li>
+        <li>予約人数： {{ $reservation['person'] }}</li>
     </ul>
     <p>ご来店お待ちしております！</p>
 </body>
