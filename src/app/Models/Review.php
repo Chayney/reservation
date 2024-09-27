331e32a57c09fcf115f1d357914dbfd967b4fc2c
Chayney <chunchunmaru33333@gmail.com>
2024-09-27 21:33:20 +0900

    レビュー用モデルの追加

---
+++ src/app/Models/Review.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 
+        'shop_id', 
+        'rating', 
+        'comment'
+    ];
+
+    public function reviewUser()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reviewShop()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+}
