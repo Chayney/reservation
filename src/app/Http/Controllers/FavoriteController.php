2bf9af453a6c6f77d38eb97931eb880f4e2918d7
Chayney <chunchunmaru33333@gmail.com>
2024-10-08 19:26:45 +0900

    お気に入り追加と削除の設定

--- src/app/Http/Controllers/FavoriteController.php
+++ src/app/Http/Controllers/FavoriteController.php
@@ -12,11 +12,8 @@ class FavoriteController extends Controller
     public function store(Request $request)
     {
         $user = Auth::user();
-
-        $shop_id = $request->shop_id;
-        
-        $favoritemark = Favorite::where('user_id', $user->id)->where('shop_id', $shop_id)->first();
-        
+        $shop_id = $request->shop_id;        
+        $favoritemark = Favorite::where('user_id', $user->id)->where('shop_id', $shop_id)->first();        
         if (!$favoritemark) {
             Favorite::create([
                 'user_id' => $user->id,
@@ -30,9 +27,7 @@ class FavoriteController extends Controller
     public function destroy(Request $request)
     {
         $user = Auth::user();
-
         $shop_id = $request->shop_id;
-
         Favorite::where('user_id', $user->id)->where('shop_id', $shop_id)->delete();
 
         return redirect()->back();
