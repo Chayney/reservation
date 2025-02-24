0d8bbf1de3bf4a959114f6ec96645cb0a8407153
Chayney <chunchunmaru33333@gmail.com>
2025-02-24 18:50:50 +0900

    管理者ページ用ヘッダーの作成

---
+++ src/resources/views/layouts/admin.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>飲食店一覧</title>
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  @yield('css')
+</head>
+
+<body>
+  <header class="header">
+    <div class="nav-humberger">
+      <input id="drawer_input" class="drawer_hidden" type="checkbox">
+      <label for="drawer_input" class="drawer_open"><span></span></label>
+    <nav class="nav_content">
+        <ul class="nav_list">
+          <li class="nav_item"><a href="/admin">Home</a></li>
+          <form action="/logout" method="post">
+            @csrf
+            <button class="logout">Logout</button>
+          </form>
+        </ul>
+    </nav>
+    </div>
+    <a class="header__logo" href="/admin">
+      Rese
+    </a>
+  </header>
+  <main>
+    @yield('content')
+  </main>
+</body>
+
+</html>
